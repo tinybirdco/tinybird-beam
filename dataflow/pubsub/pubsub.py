f649bb48dfb6b9bbb7c52b90a6c3907c878d8627
alrocar <alrocar@tinybird.co>
2021-02-25 13:13:02 +0100

    use setup instead of requirements for faster deployment

--- dataflow/pubsub/pubsub.py
+++ dataflow/pubsub/pubsub.py
@@ -1,5 +1,7 @@
 import os
+from datetime import datetime
 import time
+import json
 
 from google.cloud import pubsub_v1
 
@@ -15,8 +17,14 @@ topic_path = publisher.topic_path(PROJECT, PUBSUB_TOPIC)
 with open('invoices.json') as fp:
     line = fp.readline()
     while line:
+        obj = json.loads(line)
+        payments = obj['added_payments']
+        payments[0]['added_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        obj['added_payments'] = payments
+        obj['created_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        line = json.dumps(obj)
         # data = base64.b64decode(line)
         publisher.publish(topic_path, data=line.encode('utf-8'))
         print(line)
-        time.sleep(0.1)
+        time.sleep(0.01)
         line = fp.readline()
